667190139f87a9ba2851fe697fa1b1a42839a248
Joel Gustafsson <joelgustafsson@Joels-Mac-Studio.local>
2026-05-11 10:00:50 +0200

    Route auth flow through signin-test page

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -19,7 +19,7 @@ export default async function AppLayout({
     "use server";
 
     await signOut({
-      redirectTo: "/login"
+      redirectTo: "/signin-test"
     });
   }
 
--- lib/authorization.ts
+++ lib/authorization.ts
@@ -25,7 +25,7 @@ export async function requireUser() {
   const user = await getCurrentUser();
 
   if (!user) {
-    redirect("/login");
+    redirect("/signin-test");
   }
 
   return user;
